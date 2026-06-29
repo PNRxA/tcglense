93a5308d14c4c39a5b74174a80768458c8873300
PNRxA <pnrunham@gmail.com>
2026-06-29 12:38:22 +1000

    init

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <Button>Click me</Button>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
